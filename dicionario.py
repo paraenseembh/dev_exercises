d618916da5e09953354a91e3f7c0e0b82e7f812b
Lucas Rafael Pessoa do Nascimento <113736193+paraenseembh@users.noreply.github.com>
2024-10-02 15:37:38 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,10 @@
+#Criando dicionario e colocando dados como exemplo
+dicionario = {'nome': "Lucas",'telefone' : 3199935, 'e-mail' : "lucasrafael.pessoadn@gmail.com"}
+
+print(chaves)
+#Pedindo os dados do usuario
+dicionario["nome"] =  input("Digite o seu nome para o contato:")
+dicionario["telefone"] = input("Digite o seu numero:")
+dicionario["e-mail"] = input("Digite o seu e-mail:")
+
+print(dicionario)
